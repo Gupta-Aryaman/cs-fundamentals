fbbe6ceb4084f8f7c027c9eda29a484108e90049
Aryaman <aryaman28112002@gmail.com>
2023-08-03 20:39:48 +0530

    build files

---
+++ linux-kernel/kernel-build/README.sh
@@ -0,0 +1,3 @@
+# Follow the steps to build the kernel
+1) Run lfs.sh file to create partition table in a pendrive/disk.(disk should be of ~30GB)
+2) Partition of 100mb is created for boot partition and rest is for root partition.
